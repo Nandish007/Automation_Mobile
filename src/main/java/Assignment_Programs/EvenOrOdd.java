70c6981c001d089ab4c7407f259646b32d9448be
Nandish007 <nandisha343@gmail.com>
2025-05-16 18:28:03 +0530

    Adding a new file

---
+++ src/main/java/Assignment_Programs/EvenOrOdd.java
@@ -0,0 +1,17 @@
+package Assignment_Programs;
+
+public class EvenOrOdd {
+
+    public static void main(String[] args) {
+
+        int x=15;
+
+        if(x%2==0)
+        {
+            System.out.println("The given number "+x+" is Even");
+        }
+        else{
+            System.out.println("The given number "+x+" is Odd");
+        }
+    }
+}
