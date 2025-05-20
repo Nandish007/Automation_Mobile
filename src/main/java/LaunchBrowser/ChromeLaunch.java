b0465353d83cc515d975c485c621d2f5123c3419
Nandish007 <nandisha343@gmail.com>
2025-05-20 19:57:38 +0530

    Adding a new file

--- src/main/java/LaunchBrowser/ChromeLaunch.java
+++ src/main/java/LaunchBrowser/ChromeLaunch.java
@@ -1,9 +1,18 @@
 package LaunchBrowser;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class ChromeLaunch {
     public static void main(String[] args) {
 
     System.setProperty("webdriver.chrome.driver", "C:\\Users\\acer\\IdeaProjects\\Automation_Mobile\\chromedriver.exe");
-    Webdriver driver= new Chromedriver();
+        WebDriver driver=new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.google.com");
+
+        driver.close();
     }
 }
